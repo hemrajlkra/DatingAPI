3bfa9b410e4f91b4ec6bd104639a7d50d6aae486
hemrajlakhara <hemrajlakhara91111@gmail.com>
2026-06-03 00:47:09 +0530

    Token Logic Implementation.

---
+++ Interfaces/ITokenService.cs
@@ -0,0 +1,10 @@
+﻿using DatingAPI.Entities;
+
+namespace DatingAPI.Interfaces
+{
+    public interface ITokenService
+    {
+        public string CreateToken(AppUser user);
+        
+    }
+}
---
+++ Services/TokenService.cs
@@ -0,0 +1,37 @@
+﻿using DatingAPI.Entities;
+using DatingAPI.Interfaces;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace DatingAPI.Services
+{
+    public class TokenService(IConfiguration configuration) : ITokenService
+    {
+        public string CreateToken(AppUser user)
+        {
+            var tokenKey = configuration["token"] ?? throw new Exception("Can't get token Key");
+            if (tokenKey.Length < 64)
+                throw new Exception("Token key needs to be >= 64 characters length");
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey));
+            var claims = new List<Claim>
+            {
+                new(ClaimTypes.Email, user.Email),
+                new(ClaimTypes.NameIdentifier, user.Id)
+            };
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials= creds
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+
+
+        }
+    }
+}
